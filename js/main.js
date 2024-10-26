28d67d4b8abbd0d00e59c0ebbdfa84b3404439a3
rax1mjon <tulaganovraximjon65@gmail.com>
2024-10-26 16:04:22 +0500

    watch

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 46,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2
+        }
+    //     250: {
+    //         items: 3
+    //     },
+    //     500: {
+    //         items: 5
+    //     },
+    //     750: {
+    //         items: 7
+    //     },
+    //     1000: {
+    //         items: 9
+        // }
+    }
+})
