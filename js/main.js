84d6b9c9c0e228aa7cca31f919d9ac8cdc39bbed
rax1mjon <tulaganovraximjon65@gmail.com>
2024-10-27 08:41:53 +0500

    close  css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+
+window.addEventListener("scroll", function () {
+    shrink();
+});
+
+let navbar = document.getElementById("header");
+
+function shrink() {
+    if (scrollY > 780) {
+        navbar.classList.add("navbar-shrink");
+    } else {
+        navbar.classList.remove("navbar-shrink");
+    }
+}
+
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 46,
@@ -6,17 +21,17 @@ $('.owl-carousel').owlCarousel({
         0: {
             items: 2
         }
-    //     250: {
-    //         items: 3
-    //     },
-    //     500: {
-    //         items: 5
-    //     },
-    //     750: {
-    //         items: 7
-    //     },
-    //     1000: {
-    //         items: 9
+        //     250: {
+        //         items: 3
+        //     },
+        //     500: {
+        //         items: 5
+        //     },
+        //     750: {
+        //         items: 7
+        //     },
+        //     1000: {
+        //         items: 9
         // }
     }
 })
